40f57575c73ee4d72de46f0e6c939981eab3b3b4
Paul Krohn <pkrohn@daemonize.com>
2022-10-05 19:12:13 -0700

    make duration the number of files

--- m3u8_generator/__init__.py
+++ m3u8_generator/__init__.py
@@ -4,19 +4,28 @@ logger = logging.getLogger(__name__)
 
 
 class PlaylistGenerator(object):
+
+    header_template = "\n".join([
+        '#EXTM3U',
+        '#EXT-X-VERSION:{version}',
+        '#EXT-X-MEDIA-SEQUENCE:{sequence}',
+        '#EXT-X-TARGETDURATION:{duration}'
+    ])
+
     def __init__(self, playlist_entries=None, version=3):
-        if playlist_entries == None:
-            raise StandardError
+        if playlist_entries is None:
+            raise ValueError
 
         self.end_playlist = True
         self.playlist_entries = playlist_entries
         self.version = version
         self.sequence = 0
-        self.duration = self.duration()
+        self.duration = len(self.playlist_entries)
 
     def _generate_playlist(self):
         playlist = "{}\n{}".format(self._m3u8_header_template(), self._generate_playlist_entries())
-
+        if self.end_playlist:
+            playlist += "{}\n#EXT-X-ENDLIST"
         return playlist
 
     def _generate_playlist_entries(self):
@@ -28,25 +37,14 @@ class PlaylistGenerator(object):
 
         return playlist.replace(" ", "")
 
-
     def _generate(self):
         return self._generate_playlist()
 
     def _m3u8_header_template(self):
-        header = "#EXTM3U\n#EXT-X-VERSION:{version}\n#EXT-X-MEDIA-SEQUENCE:{sequence}\n#EXT-X-TARGETDURATION:{duration}".format(version=self.version, sequence=self.sequence, duration=self.duration).strip()
+        header = self.header_template.format(
+            version=self.version, sequence=self.sequence, duration=self.duration).strip()
 
         return header
-
-    def duration(self):
-        duration_total = 0
-        for entry in self.playlist_entries:
-            if 'duration' in entry:
-                try:
-                    duration_total += float(entry['duration'])
-                except Exception as e:
-                    logger.exception(e)
-
-        return duration_total
 
     def generate(self):
         """ This is a proxy for _generate makes it
